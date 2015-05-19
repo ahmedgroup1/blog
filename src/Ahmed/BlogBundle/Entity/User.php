2a98718d1e2c96540dd5b1fd2141b1b687f65162
ahmedgroup <ahmedgroup@ahmedgroup-PC>
2015-05-19 16:45:42 +0200

    FOSuserbundle

--- src/Ahmed/BlogBundle/Entity/User.php
+++ src/Ahmed/BlogBundle/Entity/User.php
@@ -4,13 +4,16 @@ namespace Ahmed\BlogBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+use FOS\UserBundle\Model\User as BaseUser;
+
+
 /**
  * User
  *
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="Ahmed\BlogBundle\Entity\UserRepository")
  */
-class User
+class User extends BaseUser
 {
     /**
      * @var integer
@@ -19,29 +22,9 @@ class User
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="username", type="string", length=50)
-     */
-    private $username;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="password", type="string", length=50)
-     */
-    private $password;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="role", type="smallint")
-     */
-    private $role;
-
+   
 
     /**
      * Get id
@@ -53,72 +36,11 @@ class User
         return $this->id;
     }
 
-    /**
-     * Set username
-     *
-     * @param string $username
-     * @return User
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
     
-        return $this;
-    }
 
-    /**
-     * Get username
-     *
-     * @return string 
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     * @return User
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
     
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string 
-     */
-    public function getPassword()
+    public function __construct()
     {
-        return $this->password;
-    }
-
-    /**
-     * Set role
-     *
-     * @param integer $role
-     * @return User
-     */
-    public function setRole($role)
-    {
-        $this->role = $role;
-    
-        return $this;
-    }
-
-    /**
-     * Get role
-     *
-     * @return integer 
-     */
-    public function getRole()
-    {
-        return $this->role;
+        parent::__construct();
     }
 }
